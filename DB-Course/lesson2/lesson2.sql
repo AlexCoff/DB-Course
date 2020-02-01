d2000c2bffe56d2c9aa7a6c5cbc522612f374268
Alexander Korovin <aakorovin@me.com>
2020-02-01 12:49:06 +0300

    Init repo

---
+++ DB-Course/lesson2/lesson2.sql
@@ -0,0 +1,23 @@
+-- Lesson 2
+-- Alexander Korovin
+/*
+[alex@rhel-mysql ~]$ cat .my.cnf
+[client]
+user=root
+password=Мой_пароль
+_________________________________
+[alex@rhel-mysql ~]$ ls -la
+total 44
+drwx------. 3 alex alex 4096 Feb  1 12:33 .
+drwxr-xr-x. 4 root root 4096 Feb  1 10:54 ..
+-rw-------. 1 alex alex  581 Feb  1 12:46 .bash_history
+-rw-r--r--. 1 alex alex   18 Jan 14  2019 .bash_logout
+-rw-r--r--. 1 alex alex  141 Jan 14  2019 .bash_profile
+-rw-r--r--. 1 alex alex  312 Jan 14  2019 .bashrc
+-rw-------. 1 alex alex   42 Feb  1 12:32 .my.cnf
+-rw-------. 1 alex alex  118 Feb  1 12:33 .mysql_history
+drwx------. 2 alex alex 4096 Feb  1 11:07 .ssh
+-rw-------. 1 alex alex  796 Feb  1 12:32 .viminfo
+-rw-rw-r--. 1 alex alex    0 Feb  1 11:47 yum
+-rw-r--r--. 1 alex alex  658 Dec 17  2018 .zshrc
+*/
